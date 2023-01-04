75ef311b68d9315a209be08f315bc7e3cbe6b0e3
BrianZhang1 <brian22ca@gmail.com>
2023-01-04 12:49:30 -0500

    extends JRectangle instead of JSprite

--- swingpaint/sprites/JOval.java
+++ swingpaint/sprites/JOval.java
@@ -2,7 +2,7 @@ package swingpaint.sprites;
 
 import java.awt.Rectangle;
 
-public class JOval extends JSprite {
+public class JOval extends JRectangle {
     public JOval(int x, int y, int width, int height) {
         super(x, y, width, height);
         type = "oval";
@@ -11,12 +11,5 @@ public class JOval extends JSprite {
     public JOval(Rectangle r) {
         super(r);
         type = "oval";
-    }
-
-    // Provides string representation of this sprite.
-    @Override
-    public String toString() {
-        return String.format("type=%s;x=%d;y=%d;width=%d;height=%d;color=%s",
-            type, x, y, width, height, getRGBString());
     }
 }
